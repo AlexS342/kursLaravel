889145bed11dade259a0e07270a686afdf88c76f
AlexS <ashilov84@ya.ru>
2023-06-01 21:05:01 +0500

    task completed

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hi', function () {
+    return view('hi');
+});
+
+Route::get('/inform', function () {
+    return view('inform');
+});
+
+Route::get('/news', function () {
+    return view('news');
+});
